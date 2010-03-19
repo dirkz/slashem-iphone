8d7a12e6bfa95181a22fa8fa69931cc05fb07aa3
Dirk Zimmermann <me@dirkz.com>
2010-03-19 07:43:36 +0100

    class for ascii tileset

---
+++ win/iphone/iHack/AsciiTileSet.m
@@ -0,0 +1,47 @@
+//
+//  AsciiTileSet.m
+//  SlashEM
+//
+//  Created by Dirk Zimmermann on 3/19/10.
+//  Copyright 2010 Dirk Zimmermann. All rights reserved.
+//
+
+/*
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation, version 2
+ of the License.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#import "AsciiTileSet.h"
+
+#import "hack.h"
+#import "display.h"
+
+@implementation AsciiTileSet
+
+- (id)initWithTileSize:(CGSize)ts title:(NSString *)t {
+	if (self = [super init]) {
+		tileSize = ts;
+		
+		rows = 38;
+		columns = MAX_GLYPH/rows;
+		
+		numberOfCachedImages = rows*columns;
+		cachedImages = calloc(numberOfCachedImages, sizeof(CGImageRef));
+		memset(cachedImages, 0, numberOfCachedImages*sizeof(CGImageRef));
+		title = [t copy];
+	}
+	return self;
+}
+
+@end
